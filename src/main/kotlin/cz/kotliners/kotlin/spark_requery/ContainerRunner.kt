8f31e69d5db07d277d8890699d5d4bfcc0cca788
Vaclav Souhrada <v.souhrada@gmail.com>
2017-03-11 09:33:17 +0100

    Base Project Structure with ContainerRunner.kt and ServiceRunner.kt

---
+++ src/main/kotlin/cz/kotliners/kotlin/spark_requery/ContainerRunner.kt
@@ -0,0 +1,5 @@
+package cz.kotliners.kotlin.spark_requery
+
+fun main(args: Array<String>) {
+    ServiceRunner().run()
+}
---
+++ src/main/kotlin/cz/kotliners/kotlin/spark_requery/ServiceRunner.kt
@@ -0,0 +1,9 @@
+package cz.kotliners.kotlin.spark_requery
+
+class ServiceRunner {
+
+    fun run() {
+      println("Hello Kotlin!!!")
+    }
+
+}
